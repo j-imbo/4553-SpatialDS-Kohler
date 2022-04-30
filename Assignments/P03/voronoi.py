3c3a9758c0efb656ff764323b8adbc604080cf0e
jmkohler <jameskohler1839+jimgit@gmail.com>
2022-04-30 04:07:24 -0500

    convert 3395 to WGS84

--- Assignments/P03/voronoi.py
+++ Assignments/P03/voronoi.py
@@ -2,6 +2,7 @@ import geopandas as gp
 import json
 from shapely.ops import unary_union
 from geovoronoi import voronoi_regions_from_coords, points_to_coords
+from pyproj import Transformer
 
 
 border = gp.read_file("data/us_border.geojson")
@@ -19,14 +20,15 @@ ufos = ufos.to_crs(epsg=3395)
 uInd = gp.GeoSeries(ufos["geometry"])
 
 ufoReg = []
+trfm = Transformer.from_crs("EPSG:3395", "EPSG:4326")
 for i in range(len(regions)):
-    city = {"Region #": i, "City": cty[i], "UFOs": []}
+    city = {"Region": i, "City": cty[i], "UFOs": []}
     res = uInd.within(regions[i])
     for j in range(len(res)):
-        ufo = []
         if res[j]:
-            city["UFOs"].append({j: [ufos["geometry"][j].x,
-                                     ufos["geometry"][j].y]})
+            ufo = trfm.transform(ufos["geometry"][j].x, ufos["geometry"][j].y)
+            ufo = tuple([round(x, 4) for x in ufo])
+            city["UFOs"].append({j: ufo})
     ufoReg.append(city)
 
 json.dump(ufoReg, open("voroout.json", "w"), indent=4, separators=(",", ": "))
